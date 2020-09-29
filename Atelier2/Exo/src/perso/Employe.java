1c8aca82bd064247a3dcce77a2ca75fd7cea99a9
Thunder-web <59866108+Thunder-web@users.noreply.github.com>
2020-09-29 09:45:15 +0200

    augmenterLeSalaire() + debut calculAnnuite()

--- Atelier2/Exo/src/perso/Employe.java
+++ Atelier2/Exo/src/perso/Employe.java
@@ -1,22 +1,21 @@
 package perso;
-import java.time.Year;
 import java.util.*;
 
 public class Employe extends Personne {
 
-     private GregorianCalendar dateEmbauche;
-     private int salaire;
+     private GregorianCalendar dateEmbauche2;
+     private float salaire;
 
      private Employe(Personne personne, GregorianCalendar dateEmbauche, int salaire){
           super(personne);
 
-          this.dateEmbauche = dateEmbauche;
+          this.dateEmbauche2 = dateEmbauche;
           this.salaire = salaire;
      }
      
      public static Employe createEmploye(String leNom,String lePrenom, int j, int m, int a, int numero, String rue, String code_postal, String ville, GregorianCalendar dateEmbauche, int salaire){
           
-          Employe retour;          
+          Employe retour = null;          
 
           GregorianCalendar now = new GregorianCalendar();
           GregorianCalendar dateMajorite = new GregorianCalendar(a+18,m,j);
@@ -26,12 +25,18 @@ public class Employe extends Personne {
                retour = new Employe(new Personne(leNom, lePrenom, j, m, a, numero, rue, code_postal, ville), dateEmbauche, salaire);
           }
 
-          else{
-               retour = null;
-          }
-
           return retour;
 
+     }
+
+     public void augmenterLeSalaire(float pourcentage){
+          this.salaire *= pourcentage;
+     }
+
+     public int calculAnnuite(){
+          GregorianCalendar now = new GregorianCalendar();
+
+          return now.get(GregorianCalendar.YEAR) - this.dateEmbauche2.get(GregorianCalendar.YEAR);
      }
      
 }
--- Atelier2/Exo/src/test/TestQuestion2.java
+++ Atelier2/Exo/src/test/TestQuestion2.java
@@ -4,9 +4,9 @@ import perso.Employe;
 import java.util.*;
 
 public class TestQuestion2 {
-
      public static void print(){
-          Employe employe1 = Employe.createEmploye("Dubois", "Jean-Michel", 20, 10, 2010, 69, "Maréchal Pétain", "69690", "Moncuq", new GregorianCalendar(25,11,2020), 2650);
-          System.out.println(employe1);
+          GregorianCalendar test = new GregorianCalendar(25,11,2020);
+          Employe employe1 = Employe.createEmploye("Dubois", "Jean-Michel", 20, 10, 1990, 69, "Maréchal Pétain", "69690", "Moncuq", test, 2650);
+          System.out.println(employe1 + "\n\n Annuites: " + employe1.calculAnnuite() + "an(s)");
      }
 }
